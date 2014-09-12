8c3615207a222179029e00721debc2b581167680
ZD <oooo4dzd@gmail.com>
2014-09-13 02:26:01 +0800

    initial release
    contains only Blabla page

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        sectionsColor: ['#f2f2f2', '#1bbc9b', '#7BAABE', 'whitesmoke', '#000'],
+        anchors: ['startPage', 'Web Dev', 'Android Dev', 'toDo'],
+        menu: '#Menu',
+    });
+
+});
