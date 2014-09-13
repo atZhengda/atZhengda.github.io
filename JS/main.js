22301af27a3e58153c2e42caa635e0c72c78492d
ZD <oooo4dzd@gmail.com>
2014-09-13 22:02:02 +0800

    update of task hub page

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        sectionsColor: ['#f2f2f2', '#1bbc9b', '#7BAABE', 'whitesmoke', '#000'],
+        sectionsColor: ['#f2f2f2', '#7ef5dd', '#7BAABE', 'whitesmoke', '#000'],
         anchors: ['startPage', 'Web Dev', 'Android Dev', 'toDo'],
         menu: '#Menu',
     });
+    $('#fullpage').fullpage.reBuild();
 
 });
