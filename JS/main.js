d703c97eac0a322d3c0931499bf10a7d69498fdd
ZD <oooo4dzd@gmail.com>
2014-09-13 22:27:22 +0800

    update on third page colour

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        sectionsColor: ['#f2f2f2', '#7ef5dd', '#7BAABE', 'whitesmoke', '#000'],
+        sectionsColor: ['#f2f2f2', '#7ef5dd', '#62c9f7', 'whitesmoke', '#000'],
         anchors: ['startPage', 'Web Dev', 'Android Dev', 'toDo'],
         menu: '#Menu',
     });
