b17694362b735be24940c68f43bc4c4762c9fa0b
ZD <oooo4dzd@gmail.com>
2014-09-14 22:00:38 +0800

    add in loading animation

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage({
-        sectionsColor: ['#f2f2f2', '#7ef5dd', '#62c9f7', 'whitesmoke', '#000'],
-        anchors: ['startPage', 'Web Dev', 'Android Dev', 'toDo'],
-        menu: '#Menu',
-    });
-    $('#fullpage').fullpage.reBuild();
-
+    setTimeout(function() {
+        //return;
+        $('#fullpage').fullpage({
+            sectionsColor: ['#f2f2f2', '#7ef5dd', '#62c9f7', 'whitesmoke', '#000'],
+            anchors: ['startPage', 'Web Dev', 'Android Dev', 'toDo'],
+            menu: '#Menu',
+        });
+        $('#main').show();
+        $('.mcontainer').slideUp();
+        $('#fullpage').fullpage.reBuild();
+    }, 500);
 });
