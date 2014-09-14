21f302025f30cce5cff9c0fe1d011b9869b7b562
ZD <oooo4dzd@gmail.com>
2014-09-14 22:40:49 +0800

    update loading animation

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     setTimeout(function() {
         //return;
+        $('.mcontainer').slideUp();
+        $('#main').show();
         $('#fullpage').fullpage({
             sectionsColor: ['#f2f2f2', '#62c9f7', '#7ef5dd', '#3b9dfa', 'whitesmoke'],
             anchors: ['startPage', 'Web Dev', 'VBA', 'Android Dev', 'toDo'],
             menu: '#Menu',
         });
-        $('#main').show();
-        $('.mcontainer').slideUp();
         $('#fullpage').fullpage.reBuild();
     }, 500);
 });
